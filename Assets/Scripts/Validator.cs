26a75be115fa0e07498213e316e0fac9e5b768dd
Andras Szabo <andras.szabo.fin@gmail.com>
2014-09-23 04:36:01 +0200

    First steps w/ custom attrib validation

---
+++ Assets/Scripts/Validator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using System.Text;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class MyAttribute : Attribute
+{
+	public MyAttribute()
+	{
+	}
+}
+
+public class Validator
+{
+	public static bool Validate<T>(T obj)
+	{
+		
+		// OK so GetFields really only gets fields,
+		// not properties.
+		
+		Debug.Log("Passed object has following public fields: ");
+		foreach (var f in typeof(T).GetFields())
+		{
+			Debug.Log(f.Name + " : " + f.GetValue(obj).ToString());
+			
+			var s = Attribute.GetCustomAttribute(f, typeof(MyAttribute));
+			if ( s != null )
+			{
+				Debug.Log ("Aww yiss, my attrib found!");
+			}
+		}
+		
+		Debug.Log("Passed object has following non-public fields: ");
+		foreach (var f in typeof(T).GetFields(BindingFlags.NonPublic | BindingFlags.Instance))
+		{
+			Debug.Log(f.Name + " : " + f.GetValue(obj).ToString());
+		}
+		
+		
+		return true;
+	}
+}
---
+++ Assets/Scripts/ValidatorTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ValidatorTest : MonoBehaviour {
+
+	[MyAttribute]
+	public int SomeInt;
+	
+	private int _somePrivateInt = 123;
+	protected float protFloat;
+	
+	public int OtherInt
+	{
+		get 
+		{ 	
+			return _somePrivateInt;
+		}
+	}
+
+	void Start () {
+		Validator.Validate<ValidatorTest>(this);
+	}
+}
